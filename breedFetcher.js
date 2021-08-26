b6be93a8606e498daa79c76761b5c0c34494c604
Jose Bautista <Cooldj1020@gmail.com>
2021-08-25 23:10:40 -0600

    testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,28 @@
 const request = require('request');
 
-const searchBreed = process.argv.slice(2);
+const fetchBreedDescription = (breedName, callback) => {
 
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${searchBreed}`;
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(URL, (error, response, body) => {
+    //prints error if there is one
+    if (error) {
+      return callback('request error:', error);
+      
+    }
+    //evaluate if the response went through or not
+    if (response.statusCode !== 200) {
+      return callback(`Status code: ${response.statusCode}`);
+      
+    }
+    const breed = JSON.parse(body);
 
-request(URL, (error, response, body) => {
-  //prints error if there is one
-  if (error) {
-    console.log('request error:', error);
-    return;
-  }
-  //evaluate if the response went through or not
-  if (response.statusCode !== 200) {
-    console.log(`Status code: ${response.statusCode}`);
-    return;
-  }
-  const breed = JSON.parse(body);
+    // lets the user know we didn't find the breed
+    if (breed <= 0) {
+      return callback(`the breed of ${breedName} is not found.`);
+      
+    }
+    return callback(error, breed[0].description);
+  });
+};
 
-  // lets the user know we didn't find the breed
-  if (breed <= 0) {
-    console.log(`the breed of ${searchBreed} is not found.`);
-    return;
-  }
-  console.log(breed[0].description);
-});
+module.exports = fetchBreedDescription;
